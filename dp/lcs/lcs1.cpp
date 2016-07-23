db1f818c6ed1cc8e51d403d303fb97d217fb54e9
farzicoder@theCarver <pranavkanade@acm.org>
2016-07-23 12:26:10 +0530

    recurrsive implementation of lcs is done. 1) complexity is exponential

---
+++ dp/lcs/lcs1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int lcs(char * arr1, char* arr2, int n1, int n2){
+  if(n1 == 0 || n2 == 0)
+    return 0;
+  if(arr1[n1] == arr2[n2]){
+    return 1+lcs(arr1, arr2, n1-1, n2-1);
+  }else{
+    return max(lcs(arr1, arr2, n1, n2-1), lcs(arr1, arr2,n1-1, n2));
+  }
+}
+
+int main(){
+  char arr1[] = "aggtab";
+  char arr2[] = "gxtxayb";
+  cout << "prq" << endl;
+  int p = lcs(arr1, arr2, strlen(arr1), strlen(arr2));
+  cout << "length : " << p << endl;
+  return 0;
+}
