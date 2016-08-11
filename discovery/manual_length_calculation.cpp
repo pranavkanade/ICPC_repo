03450c10d945875532dc00ef6faf2908180b9219
farziCoder <k.pranav.apk@gmail.com>
2016-08-12 03:41:48 +0530

    manually find length of an array

---
+++ discovery/manual_length_calculation.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+
+// manual length calc gives 1 more than the actual length
+int main(){
+  char str[] = "pranav";
+  lli len = sizeof(str)/sizeof(char);
+  cout << len << endl << endl;
+  return 0;
+}
