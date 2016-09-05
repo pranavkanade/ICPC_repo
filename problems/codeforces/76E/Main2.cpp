da525cdbdf28a2a4d7f24a45e41e11d94b0a52a5
farziCoder <k.pranav.apk@gmail.com>
2016-09-05 17:46:32 +0530

    done

--- problems/codeforces/76E/Main2.cpp
+++ problems/codeforces/76E/Main2.cpp
@@ -28,26 +28,10 @@ int main(){
     }
     ans = (n-1)*(x+y)*1LL;
 
-    // for(ll i = 0; i < n-1; i++){
-    //     for(ll j = i+1; j < n; j++){
-    //         temp1 += sqx[j] - 2 * points[i][0] * points[j][0];
-    //         temp2 += sqy[j] - 2 * points[i][1] * points[j][1];
-    //     }
-    //     ans += (n-i-1)*(sqx[i] + sqy[i]) + temp1 + temp2;
-    // 	temp1 = 0;
-    // 	temp2 = 0;
-    // }
-    
     for(ll i = 0; i < n-1; i++){
-      // for(ll j = i+1; j < n; j++){
-      // 	temp1 += points[j][0];
-      // 	temp2 += points[j][1];
-      // }
       temp1 -= points[i][0];
       temp2 -= points[i][1];
       ans -= 2*(temp1 * points[i][0] + temp2 * points[i][1]);
-      // temp1 = 0;
-      //temp2 = 0;
     }
     
     printf("%lld\n",ans);
