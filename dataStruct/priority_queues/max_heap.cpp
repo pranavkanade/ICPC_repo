32e9254f86f24972030f7995433e2283aa6aa08c
farziCoder <k.pranav.apk@gmail.com>
2016-09-20 21:44:18 +0530

    some changes

--- dataStruct/priority_queues/max_heap.cpp
+++ dataStruct/priority_queues/max_heap.cpp
@@ -11,11 +11,22 @@ int main(){
   que.push(mp(5,4));
   que.push(mp(3,1));
   que.push(mp(7,3));
+  priority_queue<pll, vector<pll>, greater<pll> > que1;
+ 
   pll temp;
   while(!que.empty()){
     temp = que.top();
     cout << temp.first << endl;
     que.pop();
   }
+
+  que1 = que;
+  cout << que1.empty() << endl;
+  while(!que1.empty()){
+    temp = que1.top();
+    cout << temp.first << endl;
+    que1.pop();
+  }
+  
   return 0;
 }
