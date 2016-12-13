7d8441f9de5fcd2aac1d9ae4cd9e045074c350d5
farziCoder <k.pranav.apk@gmail.com>
2016-12-13 15:00:22 +0530

    template is added

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+
+typedef vector<ll> vll;
+typedef vector<ld> vld;
+typedef vector<vll> vvll;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+typedef priority_queue<ll, vll, greater<>> minHeap;
+typedef priority_queue<ll, vll> maxHeap;
+
+#define vec vector
+
+#define mp(a, b) make_pair((a), (b))
+#define pb(a) push_back((a))
